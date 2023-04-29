45da668dfae6e87a4d3446e279b7c99905a958d7
voxj <129677054+voxj@users.noreply.github.com>
2023-04-29 15:04:10 +0300

    Added functional

---
+++ main_with_easter_eggs.py
@@ -0,0 +1,11 @@
+import os
+import webbrowser
+app_input = input("What is the directory of the file you want to open?\n")
+if app_input == "voxj":
+    webbrowser.open_new_tab('https://voxj.ml/')
+else:
+    os.system(app_input)
+if app_input == "nggyu":
+    webbrowser.open_new_tab('https://youtu.be/dQw4w9WgXcQ')
+else:
+    os.system(app_input)
