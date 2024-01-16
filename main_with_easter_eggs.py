ab2a11b12f54164a580f923c327ca7f82f1ee1da
voxj <129677054+voxj@users.noreply.github.com>
2024-01-16 09:04:39 +0300

    Changed old website to new

--- main_with_easter_eggs.py
+++ main_with_easter_eggs.py
@@ -2,7 +2,7 @@ import os
 import webbrowser
 app_input = input("What is the directory of the file you want to open?\n")
 if app_input == "voxj":
-    webbrowser.open_new_tab('https://voxj.ml/')
+    webbrowser.open_new_tab('https://voxj.github.io/')
 else:
     os.system(app_input)
 if app_input == "nggyu":
